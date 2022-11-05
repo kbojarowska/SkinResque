80f3cbbd711251b68b9ff535b777d5691284f2cf
dpionk <dariapionk13@gmail.com>
2022-11-05 14:53:20 +0100

    Added navbar component

---
+++ skin-resque-frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import './Navbar.scss';
+import Heading from '../Heading/Heading';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='pageTitle'>
+            Skin ResQ
+        </div>
+        <div className='linkList'>
+                <Link to='/'><Heading size='small'>HOME</Heading></Link>
+                <Link to='/cosmetics'><Heading size='small'>COSMETICS</Heading></Link>
+                <Link to='/login'><Heading size='small'>LOG IN</Heading></Link>
+        </div>
+    </div>
+  );
+}
+
+export default Navbar;
