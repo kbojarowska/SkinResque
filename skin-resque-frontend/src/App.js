eaf73446b836b7c3dca51dc20051c49249208fd6
dpionk <dariapionk13@gmail.com>
2022-11-05 17:27:33 +0100

    Router fix

--- skin-resque-frontend/src/App.js
+++ skin-resque-frontend/src/App.js
@@ -1,13 +1,21 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 import React from 'react'
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
+import FrontPage from './pages/FrontPage/FrontPage';
 
 function App() {
   return (
     <Router>
     <div className='App'>
       <Navbar/>
+      <Routes>
+          <Route path='/' element={<FrontPage/>}/>
+        </Routes>
     </div>
     </Router>
   );
