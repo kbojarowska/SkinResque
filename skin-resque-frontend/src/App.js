d9dd6c0f22ae50aaafea28b7db40fccb991f888a
dpionk <dariapionk13@gmail.com>
2022-11-05 16:42:35 +0100

    Added front page schema

--- skin-resque-frontend/src/App.js
+++ skin-resque-frontend/src/App.js
@@ -3,7 +3,6 @@ import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-
 function App() {
   return (
     <Router>
---
+++ skin-resque-frontend/src/pages/FrontPage/FrontPage.js
@@ -0,0 +1,18 @@
+import './FrontPage.scss';
+import photo from '../../resources/front-page-skin-photo.jpg';
+
+// const photo = require();
+
+function FrontPage() {
+  return (
+    <div className='frontPage'>
+      <div className='pink'></div>
+      <div className='white'></div>
+      <div className='green'></div>
+      <img src={photo}></img>
+      <div className='white'></div>
+    </div>
+  );
+}
+
+export default FrontPage;
