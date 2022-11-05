976ed0e9f238932f6a0348efb167311c3de9e6b7
kbojarowska <83237280+kbojarowska@users.noreply.github.com>
2022-11-05 17:06:03 +0100

    Created arrow component (#27)
    * Create eslint.yml
    
    * Update eslint.yml
    
    * Created arrow component
    
    * fix
    
    * Delete package-lock.json
    
    * Delete package.json
    
    * Removed deprecated cleaning of npm cache
    
    * Delete eslint.yml
    
    Co-authored-by: kbojarowska <kbojarowska@wp.pl>
    Co-authored-by: Bartłomiej Wujec <b.wujec.444@studms.ug.edu.pl>
    Co-authored-by: Bartłomiej Wujec <83144589+MrSpaceMan1@users.noreply.github.com>

---
+++ skin-resque-frontend/src/components/Arrow/Arrow.js
@@ -0,0 +1,26 @@
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import PropTypes from 'prop-types'
+
+
+function Arrow({left, right, onClick, color}) {
+  return (
+    left || right ?
+    <div onClick={onClick}>{left && !right ? <FiChevronLeft size={45} color={color}/> : <FiChevronRight size={45} color={color}/>}</div>
+    : null
+  )
+}
+
+export default Arrow
+
+const requiredPropsCheck = (props, componentName) => {
+  if (!props.left && !props.right) {
+    return new Error(`One of 'left' or 'right' is required by '${componentName}' component.`)
+  }
+}
+
+Arrow.propTypes = {
+  left: requiredPropsCheck,
+  right: requiredPropsCheck,
+  onClick: PropTypes.func,
+  color: PropTypes.string
+}
