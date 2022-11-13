73fac8b4fe11b7916bd6a70aaea42ae9c2fad788
Daria Pionk <dariapionk13@gmail.com>
2022-11-13 22:33:51 +0100

    Changes after code review

--- skin-resque-frontend/src/pages/FrontPage/FrontPage.js
+++ skin-resque-frontend/src/pages/FrontPage/FrontPage.js
@@ -12,47 +12,52 @@ import Heading from '../../components/Heading/Heading';
 import Text from '../../components/Text/Text';
 
 function FrontPage() {
-  return (
-    <div className='front-page'>
-      <div className='pink'><img src={coniferBush}/></div>
-      <div className='about-dots'>
-      <div className='page-about'>
-        <Heading size='medium'>Page Information</Heading>
-        <Text size='small'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-      </div>
-      <div className='pink-dots'>
-        <img src={pinkDots}></img>
-      </div>
-      </div>
-      <div className='white'></div>
-      <div className='photo-button-list linkList'>
-        <div className='photo-button'>
-          <img src={skintypeTestPhoto}></img>
-        <Button><Link to='/skintype-test'>Skintype Test</Link></Button>
-        </div>
-        <div className='photo-button'>
-        <img src={colorTestPhoto}></img>
-          <Button><Link to='/color-test'>Color Test</Link></Button>
-        </div>
-        <div className='photo-button'>
-        <img src={cosmeticRecipesPhoto}></img>
-          <Button><Link to='/cosmetic-list'>Cosmetic Recipes</Link></Button>
-        </div>
-      </div>
-      <div className='water-drops'>
-      <img src={waterDrops}></img>
-      <img src={waterDrops}></img>
-      <img src={waterDrops} ></img>
-      </div>
-      <div className='green'></div>
-      <img src={photo} className='skin-photo'></img>
-      <div className='white'></div>
-    </div>
-  );
+	return (
+		<div className='front-page'>
+			<div className='pink'>
+				<div className='conifer-bush-about'>
+					<img src={coniferBush} />
+					<div className='about'>
+						<div className='page-about'>
+							<Heading size='medium'>Page Information</Heading>
+							<Text size='small'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+								ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+								ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+								in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+								sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='white'>
+				<div className='pink-dots'>
+					<img src={pinkDots}></img>
+				</div>
+			</div>
+			<div className='photo-button-list linkList'>
+				<div className='photo-button'>
+					<img src={skintypeTestPhoto}></img>
+					<Button><Link to='/skintype-test'>Skintype Test</Link></Button>
+				</div>
+				<div className='photo-button'>
+					<img src={colorTestPhoto}></img>
+					<Button><Link to='/color-test'>Color Test</Link></Button>
+				</div>
+				<div className='photo-button'>
+					<img src={cosmeticRecipesPhoto}></img>
+					<Button><Link to='/cosmetic-list'>Cosmetic Recipes</Link></Button>
+				</div>
+			</div>
+			<div className='water-drops'>
+				<img src={waterDrops}></img>
+				<img src={waterDrops}></img>
+				<img src={waterDrops} ></img>
+			</div>
+			<div className='green'></div>
+			<img src={photo} className='skin-photo'></img>
+			<div className='white'></div>
+		</div>
+	);
 }
 
 export default FrontPage;
