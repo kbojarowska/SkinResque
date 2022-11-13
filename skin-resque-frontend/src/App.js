b83d88dabc5743566bedd9d5f1600b24d96fa96a
kbojarowska <kbojarowska@wp.pl>
2022-11-13 21:14:37 +0100

    fixed details size"
    "

--- skin-resque-frontend/src/App.js
+++ skin-resque-frontend/src/App.js
@@ -7,7 +7,6 @@ import React from 'react'
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 import FrontPage from './pages/FrontPage/FrontPage';
-import RecipeDetails from './pages/RecipeDetails/RecipeDetails';
 
 function App() {
   return (
@@ -16,7 +15,6 @@ function App() {
       <Navbar/>
       <Routes>
           <Route path='/' element={<FrontPage/>}/>
-          <Route path='/cosmetics' element={<RecipeDetails/>}/>
         </Routes>
     </div>
     </Router>
