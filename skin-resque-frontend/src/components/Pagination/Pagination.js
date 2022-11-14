f0649fffcc11b95aeacac5ec91de04330a368d50
dpionk <83178883+dpionk@users.noreply.github.com>
2022-11-14 15:10:06 +0100

    Create cosmetics catalogue (#47)
    * Added cosmetics catalogue
    
    * Added cosmetics catalogue
    
    * Updated box shadow and changed width and height of image

--- skin-resque-frontend/src/components/Pagination/Pagination.js
+++ skin-resque-frontend/src/components/Pagination/Pagination.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import Heading from '../Heading/Heading';
 import './Pagination.scss';
 
-export default function Pagination({ url, color, size }) {
+export default function Pagination({ url, color, size, currentPage }) {
 
 	const pagesList = (new Array(size)).fill(0).map((_, index) => {
 		const pageNumber = index + 1;
 		return (
 			<Link to={`${url}/${pageNumber}`} key={pageNumber} className={color}>
-				<Heading key={pageNumber}>
+				<Heading key={pageNumber} className={pageNumber === parseInt(currentPage) ? 'current': null}>
 					{pageNumber}
 				</Heading>
 			</Link>
@@ -26,5 +26,6 @@ export default function Pagination({ url, color, size }) {
 Pagination.propTypes = {
 	url: PropTypes.string,
 	color: PropTypes.oneOf(['green', 'gray']),
-	size: PropTypes.number
+	size: PropTypes.number,
+	currentPage: PropTypes.string
 };
---
+++ skin-resque-frontend/src/pages/CosmeticsCatalogue/CosmeticsCatalogue.js
@@ -0,0 +1,100 @@
+import { Link, useParams } from 'react-router-dom';
+import './CosmeticsCatalogue.scss';
+import Heading from '../../components/Heading/Heading';
+import Text from '../../components/Text/Text';
+import Pagination from '../../components/Pagination/Pagination'
+
+const cosmetics = [
+	{
+		'id': 1,
+		'name': 'rosemary cream',
+		'description': `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+      sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+		'photo': '/images/cream.png',
+	},
+	{
+		'id': 2,
+		'name': 'rosemary cream',
+		'description': `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+      sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+		'photo': '/images/cream.png',
+	},
+	{
+		'id': 3,
+		'name': 'rosemary cream',
+		'description': `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+      sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+		'photo': '/images/cream.png',
+	},
+	{
+		'id': 4,
+		'name': 'rosemary cream',
+		'description': `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+      sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+		'photo': '/images/cream.png',
+	},
+	{
+		'id': 5,
+		'name': 'rosemary cream',
+		'description': `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+      sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+		'photo': '/images/cream.png',
+	}
+]
+
+function CosmeticsCatalogue() {
+
+	const { currentPage } = useParams();
+
+	const cosmeticsList = cosmetics.map((cosmetic) => {
+		return (
+			<Link to={`/cosmetics/${cosmetic.id}`} key={cosmetic.id}>
+				<button className='cosmetic'>
+					<img src={cosmetic.photo}></img>
+					<Text>{cosmetic.name}</Text>
+				</button>
+			</Link >
+		)
+	})
+
+	return (
+		<div className='cosmetics-page'>
+			<div className='pink' />
+			<img src='/images/pink-spot.png' className='pink-spot'></img>
+			<img src='/images/small-pink-dots.png' className='small-pink-dots'></img>
+			<img src='/images/yellow-spot.png' className='yellow-spot'></img>
+			<div className='catalogue'>
+				<div className='page-title'>
+					<Heading size='large'>Cosmetics</Heading>
+				</div>
+				<div className='cosmetics-list'>
+					{cosmeticsList}
+				</div>
+				<div className='pagination'>
+					<Pagination size={15} url='/cosmetics/page' color='gray' currentPage={currentPage} />
+				</div>
+				<div className='white'>
+					<img src='/images/rosemary-sprig.png' className='rosemary-sprig-left'></img>
+					<img src='/images/rosemary-sprig.png' className='rosemary-sprig-right'></img>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default CosmeticsCatalogue;
