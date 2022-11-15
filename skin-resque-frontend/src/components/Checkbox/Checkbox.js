c7ea350cab372bd7433f99e3720592aa6a984f0d
dpionk <dariapionk13@gmail.com>
2022-11-15 21:58:58 +0100

    Created checkbox component

---
+++ skin-resque-frontend/src/components/Checkbox/Checkbox.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import Text from '../Text/Text';
+import './Checkbox.scss';
+
+function Checkbox({checked, children}) {
+	const [isChecked, setIsChecked] = useState(checked);
+
+	return (
+		<div className='checkbox-wrapper'>
+			<input type='checkbox' onChange={() => setIsChecked((checked) => !checked)}/>
+			<Text>{children}</Text>
+		</div>
+	);
+};
+
+export default Checkbox;
+
+Checkbox.propTypes = {
+  checked: PropTypes.bool
+};
