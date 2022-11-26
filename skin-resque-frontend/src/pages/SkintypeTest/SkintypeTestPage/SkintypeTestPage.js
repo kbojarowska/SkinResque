fb76cbbc106c6b0f847a168d4e5ff6c782c01db7
Daria Pionk <dariapionk13@gmail.com>
2022-11-26 15:09:45 +0100

    Created skin type questions page

--- skin-resque-frontend/src/pages/SkintypeTest/SkintypeTestPage/SkintypeTestPage.js
+++ skin-resque-frontend/src/pages/SkintypeTest/SkintypeTestPage/SkintypeTestPage.js
@@ -1,20 +1,25 @@
+import { useParams } from 'react-router-dom';
 import Heading from '../../../components/Heading/Heading';
 import './SkintypeTestPage.scss';
 import Info from '../Info/Info';
+import Questions from '../Questions/Questions';
 
 function SkintypeTestPage() {
+
+	const { questionNumber } = useParams();
 
 	return (
 		<div className='skintype-test-page'>
 			<div className='page-title'>
-				<Heading size='large'>Skintype test</Heading>
+				<Heading size='large'>Skintype Test</Heading>
 			</div>
 			<div className='pink' />
 			<img src='/images/pink-spot.png' className='pink-spot'></img>
 			<img src='/images/small-pink-dots.png' className='small-pink-dots'></img>
 			<img src='/images/yellow-spot.png' className='yellow-spot'></img>
 			<div className='skintype-test'>
-				<Info />
+				{!questionNumber && <Info />}
+				{questionNumber && <Questions currentQuestion={questionNumber} numberOfQuestions={15}/>}
 			</div>
 			<div className='white'>
 			</div>
