211aca507b1fab931d47707db5220fed2e20bd6a
Daria Pionk <dariapionk13@gmail.com>
2023-01-08 17:10:52 +0100

    Added skin type test questions and initial functionality

--- skin-resque-frontend/src/components/Checkbox/Checkbox.js
+++ skin-resque-frontend/src/components/Checkbox/Checkbox.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import Text from '../Text/Text';
 import './Checkbox.scss';
 
-function Checkbox({checked, children}) {
+function Checkbox({ checked, children, onClick }) {
 	const [isChecked, setIsChecked] = useState(checked);
 
 	return (
 		<div className='checkbox-wrapper'>
-			<input type='checkbox' onChange={() => setIsChecked((prev) => !prev)} value={isChecked}/>
+			<input type='checkbox' onChange={() => {onClick(); setIsChecked((prev) => !prev)}} value={isChecked} className={isChecked ? 'checked' : ''}/>
 			<Text>{children}</Text>
 		</div>
 	);
--- skin-resque-frontend/src/pages/SkintypeTest/Questions/Questions.js
+++ skin-resque-frontend/src/pages/SkintypeTest/Questions/Questions.js
@@ -4,93 +4,243 @@ import './Questions.scss';
 
 function Questions() {
 
-
-	const questions = [
+	let questions = [
 		{
 			'id': 1,
-			'question': 'Lorem ipsum?',
+			'question': 'Does your skin feel tight and dry after washing your face?',
 			'answers': [
 				{
 					'id': 1,
-					'answer': 'Lorem ipsum'
+					'answer': 'Yes, my whole skin feel like it',
+					'type': 'dry',
+					'checked': false
 				},
 				{
 					'id': 2,
-					'answer': 'Dolor Sit Amet'
+					'answer': 'Only on cheeks',
+					'type': 'mixed',
+					'checked': false
 				},
 				{
 					'id': 3,
-					'answer': 'nie'
+					'answer': 'No, it does not feel like it',
+					'type': ['oily', 'normal'],
+					'checked': false
 				}
 			]
 		},
 		{
 			'id': 2,
-			'question': 'Lorem ipsum2?',
+			'question': 'Is your skin reactive to changes of seasons?',
 			'answers': [
 				{
 					'id': 4,
-					'answer': 'Lorem ipsum2'
+					'answer': 'No',
+					'type': ['normal'],
+					'checked': false
 				},
 				{
 					'id': 5,
-					'answer': 'Dolor Sit Amet2'
-				},
-				{
-					'id': 6,
-					'answer': 'nie2'
+					'answer': 'Yes',
+					'type': ['oily', 'dry', 'mixed'],
+					'checked': false
 				}
 			]
 		},
 		{
 			'id': 3,
-			'question': 'Lorem ipsum3?',
+			'question': 'Do you have a lot of imprefections like pimples, blackheads?',
 			'answers': [
 				{
+					'id': 6,
+					'answer': 'No, I rarely have any imperfection on my skin',
+					'type': ['dry', 'normal'],
+					'checked': false
+				},
+				{
 					'id': 7,
-					'answer': 'Lorem ipsum3'
+					'answer': 'Yes, but only in T-zone',
+					'type': 'mixed',
+					'checked': false
 				},
 				{
 					'id': 8,
-					'answer': 'Dolor Sit Amet3'
-				},
+					'answer': 'Yes, on my whole face, including cheeks',
+					'type': 'oily',
+					'checked': false
+				}
+			]
+		},
+		{
+			'id': 4,
+			'question': 'Do you have a problem with shiny face after a whole day?',
+			'answers': [
 				{
 					'id': 9,
-					'answer': 'nie3'
+					'answer': 'No, my skin is rather dull',
+					'type': ['dry', 'normal'],
+					'checked': false
+				},
+				{
+					'id': 10,
+					'answer': 'Yes, but only in T-zone',
+					'type': 'mixed',
+					'checked': false
+				},
+				{
+					'id': 11,
+					'answer': 'Yes, on my whole face, including cheeks',
+					'type': 'oily',
+					'checked': false
 				}
+			]
+		},
+		{
+			'id': 5,
+			'question': 'Does your skin itch sometimes?',
+			'answers': [
+				{
+					'id': 12,
+					'answer': 'Yes, it does happen sometimes',
+					'type': 'dry',
+					'checked': false
+				},
+				{
+					'id': 13,
+					'answer': 'Yes, but only on my cheeks',
+					'type': 'mixed',
+					'checked': false
+				},
+				{
+					'id': 14,
+					'answer': 'No, not really',
+					'type': ['oily', 'normal'],
+					'checked': false
+				}
+			]
+		},
+		{
+			'id': 6,
+			'question': 'Are you getting redness on your skin?',
+			'answers': [
+				{
+					'id': 15,
+					'answer': 'Yes, it does happen sometimes',
+					'type': 'dry',
+					'checked': false
+				},
+				{
+					'id': 16,
+					'answer': 'Yes, but only on my cheeks',
+					'type': 'mixed',
+					'checked': false
+				},
+				{
+					'id': 17,
+					'answer': 'No, not really',
+					'type': 'normal',
+					'checked': false
+				},
+				{
+					'id': 18,
+					'answer': 'Only because of my pimples',
+					'type': 'oily',
+					'checked': false
+				}
+			]
+		},
+		{
+			'id': 7,
+			'question': 'How would you describe your pores?',
+			'answers': [
+				{
+					'id': 19,
+					'answer': 'They are hardly visible',
+					'type': ['dry', 'normal'],
+					'checked': false
+				},
+				{
+					'id': 20,
+					'answer': 'They are very visible',
+					'type': 'oily',
+					'checked': false
+				},
+				{
+					'id': 21,
+					'answer': 'They are hardly visible on cheeks and very visible in T-zone',
+					'type': ['mixed'],
+					'checked': false
+				},
+			]
+		},
+		{
+			'id': 8,
+			'question': 'How prone to wrinkles are you?',
+			'answers': [
+				{
+					'id': 22,
+					'answer': 'Very',
+					'type': 'dry',
+					'checked': false
+				},
+				{
+					'id': 23,
+					'answer': 'Not really',
+					'type': 'mixed',
+					'checked': false
+				},
+				{
+					'id': 24,
+					'answer': "I can't really tell",
+					'type': '',
+					'checked': false
+				},
 			]
 		},
 	]
 
+	console.log(questions);
+	const getSkinTypeAccordingToGivenAnswers = (answers) => {
+		return answers.sort((a, b) =>
+			answers.filter(v => v === a).length
+			- answers.filter(v => v === b).length
+		).pop();
+	}
+
 	const [currentQuestion, setCurrentQuestion] = useState(1);
+	const [answers, setAnswers] = useState([]);
 
 	const answersToDisplay = questions.filter((question) => {
 		return question.id == currentQuestion
 	})[0].answers.map((answer) => {
-			return (<div className='answer' key={answer.id}>
-			<Checkbox checked={false}>{answer.answer}</Checkbox>
-					</div>
-			)
+		return (<div className='answer' key={answer.id}>
+			<Checkbox checked={answer.checked} onClick={() => {answer.checked = !answer.checked;}}>{answer.answer}</Checkbox>
+		</div>
+		)
 	})
+
+	const questionToDisplay = questions.filter((question) => {
+		return question.id == currentQuestion
+	})[0].question
 
 	return (
 		<div className='questions-pagination'>
 			<div className='questions-container'>
 				<div className='questions'>
 					<div className='question-number'>
-						<Heading>Question {currentQuestion}.</Heading>
+						<Heading>{currentQuestion}. {questionToDisplay}</Heading>
 					</div>
 					<div className='answers-arrows'>
 						<div className='arrows'>
 							<div className='arrow'>
-								{currentQuestion != 1 ? <Arrow left onClick={() => setCurrentQuestion(currentQuestion - 1)}/> : <div className='hide'><Arrow left /></div>}
+								{currentQuestion != 1 ? <Arrow left onClick={() => setCurrentQuestion(currentQuestion - 1)} /> : <div className='hide'><Arrow left /></div>}
 							</div>
 							<div className='answers'>
-							{answersToDisplay}
+								{answersToDisplay}
 							</div>
 							<div className='arrow'>
 								{currentQuestion != Object.keys(questions).length ? <Arrow right onClick={() => setCurrentQuestion(currentQuestion + 1)} /> : <div className='hide'><Arrow right /></div>}
-								{currentQuestion == Object.keys(questions).length && <Button className='submit-answers'>Submit</Button>}
+								{currentQuestion == Object.keys(questions).length && <Button className='submit-answers' onClick={() => console.log(getSkinTypeAccordingToGivenAnswers(answers))}>Submit</Button>}
 							</div>
 						</div>
 					</div>
