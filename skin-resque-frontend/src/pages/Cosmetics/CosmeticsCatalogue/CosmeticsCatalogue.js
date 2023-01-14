49a8bd8021db9d34bc76d7eb2ec38d32dca73284
dpionk <83178883+dpionk@users.noreply.github.com>
2023-01-14 13:37:08 +0100

    Connected frontend with backend (#105)

--- skin-resque-frontend/src/pages/Cosmetics/CosmeticsCatalogue/CosmeticsCatalogue.js
+++ skin-resque-frontend/src/pages/Cosmetics/CosmeticsCatalogue/CosmeticsCatalogue.js
@@ -1,68 +1,29 @@
+import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import './CosmeticsCatalogue.scss';
+import axios from 'axios';
 import { Heading, Pagination, Text } from '../../../components';
-
-
-const cosmetics = [
-	{
-		'id': 1,
-		'name': 'rosemary cream',
-		'description': `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-      sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-		'photo': '/images/cream.png',
-	},
-	{
-		'id': 2,
-		'name': 'rosemary cream',
-		'description': `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-      sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-		'photo': '/images/cream.png',
-	},
-	{
-		'id': 3,
-		'name': 'rosemary cream',
-		'description': `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-      sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-		'photo': '/images/cream.png',
-	},
-	{
-		'id': 4,
-		'name': 'rosemary cream',
-		'description': `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-      sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-		'photo': '/images/cream.png',
-	},
-	{
-		'id': 5,
-		'name': 'rosemary cream',
-		'description': `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-      sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-		'photo': '/images/cream.png',
-	}
-]
+import './CosmeticsCatalogue.scss';
 
 function CosmeticsCatalogue() {
 
 	const { currentPage } = useParams();
+	const API_URL = 'http://localhost:5000/'
+
+	const [cosmetics, setCosmetics] = useState([]);
+
+	useEffect(() => {
+		console.log(`${API_URL}cosmetics?size=10&page=${currentPage}`)
+		axios.get(`${API_URL}cosmetics?size=10&page=${currentPage}`).then((response) => {
+			setCosmetics(response.data);
+		}).catch((error) => {
+			console.error(error);
+			alert('Something went wrong while downloading cosmetics data')
+		})
+	}, []);
 
 	const cosmeticsList = cosmetics.map((cosmetic) => {
 		return (
-			<Link to={`/cosmetics/${cosmetic.id}`} key={cosmetic.id}>
+			<Link to={`/cosmetics/${cosmetic._id}`} key={cosmetic.id}>
 				<button className='cosmetic'>
 					<img src={cosmetic.photo}></img>
 					<Text>{cosmetic.name}</Text>
@@ -78,7 +39,7 @@ function CosmeticsCatalogue() {
 					<Heading size='large'>Cosmetics</Heading>
 				</div>
 				<div className='cosmetics-list'>
-					{cosmeticsList}
+					{cosmetics && cosmeticsList ?  cosmeticsList : <Text size='small'>No cosmetics to display</Text>}
 				</div>
 				<div className='pagination'>
 					<Pagination size={15} url='/cosmetics/page' color='gray' currentPage={currentPage} />
