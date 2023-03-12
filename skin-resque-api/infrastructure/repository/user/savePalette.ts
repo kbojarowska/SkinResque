9e9e1fca939d9ec57601dee8bf8f7106ff95dae1
Daria Pionk <dariapionk13@gmail.com>
2023-03-12 16:15:24 +0100

    Implemented user post routes for cosmetics and palettes

--- skin-resque-api/infrastructure/repository/user/savePalette.ts
+++ skin-resque-api/infrastructure/repository/user/savePalette.ts
@@ -2,13 +2,13 @@ import { User } from '../../../domain/models/index.js';
 import DB from '../../database_abstraction/index.js';
 import { UpdateReturns } from '../../database_abstraction/types.js';
 
-export const saveCosmetics = async (id: string, cosmeticId: string): Promise<UpdateReturns> => {
+export const savePalette = async (id: string, paletteId: string): Promise<UpdateReturns> => {
     return DB.update(
         User,
         { _id: id },
         {
             // @ts-ignore
-            $push: { saved_cosmetics: cosmeticId },
+            $push: { saved_palettes: paletteId },
         },
         { upsert: false }
     );
