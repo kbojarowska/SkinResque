4059b71f33b4acfcbf64b0e48290f065d7f4c052
kbojarowska <kbojarowska@wp.pl>
2023-03-18 18:19:32 +0100

    created function for face detect

--- skin-resque-python-api/main.py
+++ skin-resque-python-api/main.py
@@ -9,7 +9,7 @@ def face_detect(photo_path):
     face_cascade = cv2.CascadeClassifier("haarcascade_fontalface_default.xml")
 
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(
+    faces_detected = face_cascade.detectMultiScale(
         gray_img,
         scaleFactor=1.1,
         minNeighbors=5,
@@ -17,7 +17,7 @@ def face_detect(photo_path):
         flags=cv2.CASCADE_SCALE_IMAGE
     )
 
-    for (x, y, w, h) in faces:
+    for (x, y, w, h) in faces_detected:
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     cv2.imshow("Faces found", image)
