c911027890159b9c43d7c2f4b424fd5155ca596f
kbojarowska <83237280+kbojarowska@users.noreply.github.com>
2023-03-19 11:47:46 +0100

    Face detect (#152)
    * set up python server
    
    * created function for face detect
    
    * created function for face detect
    
    ---------
    
    Co-authored-by: kbojarowska <kbojarowska@wp.pl>

--- skin-resque-python-api/main.py
+++ skin-resque-python-api/main.py
@@ -1,6 +1,28 @@
 from flask import Flask
+import cv2
+
 
 app = Flask(__name__)
+
+
+def face_detect(photo_path):
+    image = cv2.imread(photo_path)
+    face_cascade = cv2.CascadeClassifier("haarcascade_fontalface_default.xml")
+
+    gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces_detected = face_cascade.detectMultiScale(
+        gray_img,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    for (x, y, w, h) in faces_detected:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("Faces found", image)
+    cv2.waitKey(0)
 
 
 @app.route('/')
