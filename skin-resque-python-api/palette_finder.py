1355fd73daa1b48c60304f4e360bb18f7c102724
Daria Pionk <dariapionk13@gmail.com>
2023-03-22 17:20:45 +0100

    Implemented api requests for palettes

---
+++ skin-resque-python-api/palette_finder.py
@@ -0,0 +1,13 @@
+import requests
+
+URL = 'http://www.colourlovers.com/api/palettes?format=json&hex='
+
+headers = {
+    'User-Agent': 'Mozilla/5.0'
+}
+
+def palette_finder(hexColor):
+	response = requests.get(URL + hexColor, headers=headers)
+	response.raise_for_status() 
+	if response.status_code != 204:
+		return response.json()
