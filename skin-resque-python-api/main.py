50c7494e587f0e7b6a3e479f56d371505a694a5e
dpionk <83178883+dpionk@users.noreply.github.com>
2023-03-25 19:58:57 +0100

    Finished python endpoint to get palettes (#172)

--- skin-resque-python-api/main.py
+++ skin-resque-python-api/main.py
@@ -4,29 +4,32 @@ import cv2
 import numpy as np
 from flask import Flask, request, jsonify, abort
 from face_detect import face_detect
+from color_finder import color_finder
+from palette_finder import palette_finder
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+	return 'Hello, World!'
 
 
 @app.route('/palettes', methods=['POST'])
 def create_palettes():
-    encoded_image = request.json['image']
+	encoded_image = request.json['image']
 
-    decoded_image = np.frombuffer(base64.b64decode(encoded_image), dtype=np.uint8)
-    decoded_image = cv2.imdecode(decoded_image, cv2.IMREAD_COLOR)
+	decoded_image = np.frombuffer(base64.b64decode(encoded_image), dtype=np.uint8)
+	decoded_image = cv2.imdecode(decoded_image, cv2.IMREAD_COLOR)
 
-    return_img = face_detect(decoded_image)
+	return_img = face_detect(decoded_image)
 
-    if np.any(return_img):
-        return jsonify({'message': 'success'})
-    else:
-        abort(400, 'Invalid data provided')
+	if np.any(return_img):
+		found_color = color_finder(return_img)
+		palettes = palette_finder(found_color)
+		return jsonify(palettes)
+	else:
+		abort(400, 'Invalid data provided')
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5100)
+if __name__ == '__main__':
+	app.run(host="0.0.0.0", port=5100)
