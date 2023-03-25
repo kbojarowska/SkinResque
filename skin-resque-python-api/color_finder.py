8bc9ba767434b270b1f32f5e948e76e40c233c3f
dpionk <83178883+dpionk@users.noreply.github.com>
2023-03-25 13:15:15 +0100

    Implemented api requests for palettes (#168)

--- skin-resque-python-api/color_finder.py
+++ skin-resque-python-api/color_finder.py
@@ -1,12 +1,15 @@
 import cv2 as cv
 import numpy as np
 
+def rgbToHex(RGGBcolor):
+    return '%02x%02x%02x' % RGGBcolor
 
 def color_finder(photo_path):
-    image = cv.imread(photo_path)
-    image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-    color_photo = image.copy()
-    unique, counts = np.unique(color_photo.reshape(-1, 3), axis=0, return_counts=True)
-    unique[np.argmax(counts)] = color_photo[:, :, 0], color_photo[:, :, 1], color_photo[:, :, 2]
-    RGBcolor = (color_photo[0][0][0], color_photo[0][0][1], color_photo[0][0][2])
-    return RGBcolor
+	image = cv.imread(photo_path)
+	image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+	color_photo = image.copy()
+	unique, counts = np.unique(color_photo.reshape(-1, 3), axis=0, return_counts=True)
+	color_photo[:,:,0], color_photo[:,:,1], color_photo[:,:,2] = unique[np.argmax(counts)]
+	RGBcolor = (color_photo[0][0][0], color_photo[0][0][1], color_photo[0][0][2])
+	HEXcolor = rgbToHex(RGBcolor)
+	return HEXcolor
