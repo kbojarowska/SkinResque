69e0693895ac0bd8765e5cea896b56250db663c3
kbojarowska <kbojarowska@wp.pl>
2023-04-01 11:14:03 +0200

    used axios

--- skin-resque-frontend/src/ducks/UploadImage/actions.js
+++ skin-resque-frontend/src/ducks/UploadImage/actions.js
@@ -1,32 +1,23 @@
+import axios from 'axios';
 import types from './types';
 
 export const uploadImage = (imageFile) => {
-return (dispatch) => {
-const reader = new FileReader();
-reader.readAsDataURL(imageFile);
+  return (dispatch) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(imageFile);
 
-reader.onload = () => {
-  const base64Image = reader.result;
+    reader.onload = () => {
+      const base64Image = reader.result;
 
-  fetch('http://localhost:5100/palettes', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ image: base64Image })
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      dispatch({ type: types.UPLOAD_IMAGE_SUCCESS, payload: data, img: base64Image });
-    })
-    .catch((error) => {
-      dispatch({ type: types.UPLOAD_IMAGE_FAILURE, payload: error });
-    });
-};
-};
+      axios.post('http://localhost:5100/palettes', { image: base64Image }, {
+        headers: { 'Content-Type': 'application/json' }
+      })
+        .then((response) => {
+          dispatch({ type: types.UPLOAD_IMAGE_SUCCESS, payload: response.data, img: base64Image });
+        })
+        .catch((error) => {
+          dispatch({ type: types.UPLOAD_IMAGE_FAILURE, payload: error });
+        });
+    };
+  };
 };
